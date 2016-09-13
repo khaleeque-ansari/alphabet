380b7bda872fbdc95ebcb025d2070b7a7af50158
Khaleeque Ansari <klickans@gmail.com>
2016-09-14 00:51:55 +0530

    first commit

---
+++ alphabet/local_settings.py
@@ -0,0 +1,25 @@
+__author__ = 'khaleeque'
+import os
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': ['/Users/khaleeque/Clients/Khaleeque/khaleequeansari_com/alphabet/templates'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
