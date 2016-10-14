d80d0efbf98beade4d34b3f9bd203cd0f1630d12
Khaleeque Ansari <klickans@gmail.com>
2016-10-15 01:57:51 +0530

    Added resume

--- basics/urls.py
+++ basics/urls.py
@@ -6,5 +6,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^index.html', views.index, name='index'),
+    url(r'^resume.html', views.resume, name='resume'),
+    url(r'^resume.pdf', views.resume_pdf, name='resume_pdf'),
 
 ]
--- basics/views.py
+++ basics/views.py
@@ -6,3 +6,12 @@ def index(request):
     context = {}
     return render(request, 'basics/index.html', context)
 
+
+def resume(request):
+    context = {}
+    return render(request, 'basics/resume.html', context)
+
+
+def resume_pdf(request):
+    resume_data = open("static/basics/downloads/Khaleeque_Ansari_Resume.pdf", "rb").read()
+    return HttpResponse(resume_data, content_type="application/pdf")
