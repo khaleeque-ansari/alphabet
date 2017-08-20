3d55776211af8e8a2f32ff5bfeffcedf99219096
Wasique Ansari <wasique3@gmail.com>
2017-08-21 02:49:09 +0530

    Update settings.py

--- alphabet/settings.py
+++ alphabet/settings.py
@@ -25,7 +25,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%&b5)w4=0zzx4d029ln9#ndx=o*8w3kug+vr0-x_b$%-$ar3+4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True 
+DEBUG = True
+
+PREPEND_WWW = True
 
 ALLOWED_HOSTS = ['www.khaleequeansari.com']
 
@@ -50,8 +52,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
-PREPEND_WWW = True
 
 ROOT_URLCONF = 'alphabet.urls'
 
